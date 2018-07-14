5345df754a1283772cccf975ee469a62b48aa075
Balraj Singh <balraajbrar@gmail.com>
2018-07-14 14:11:08 +0530

    Added Registration Dao

--- EventManagementSystem/src/com/project/dao/RegistrationDaoImp.java
+++ EventManagementSystem/src/com/project/dao/RegistrationDaoImp.java
@@ -5,45 +5,40 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 
-import com.project.pojo.Registration;
-
-public class RegistrationDaoImp {
-	
+public class RegistrationDaoImp implements RegistrationDao{
+	@Override
 	public boolean insertRegistration(int employeeId,int eventId) throws SQLException, ClassNotFoundException
 	{
-		Class.forName("org.postgresql.Driver");
-		Connection connection=DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/eventmanagement", 
-				"postgres", "admin");
+		Connection connection = DBConnection.getDBConnection();
 		PreparedStatement pstmt = connection.prepareStatement("insert into registrations values(?,?)");
 		pstmt.setInt(1, employeeId);
 		pstmt.setInt(2, eventId);
 		int result = pstmt.executeUpdate();
 		
 		if (result > 0){
-				return true;
+			return true;
 		}
 		else{
-				return false;
+			return false;
 		}
 	}
 	
-	
+	@Override
     public boolean deleteRegistration(int employeeId,int eventId) throws ClassNotFoundException, SQLException
     {
-    	Connection connection = null;
-		
-		Class.forName("org.postgresql.Driver");
-		connection=DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/eventmanagement", "postgres", "admin");
-		Statement smt=connection.createStatement();
-		String s="delete from registrations where employee_Id ="+employeeId+"and event_id="+eventId;
-		int check=smt.executeUpdate(s);
-
-		if (check> 0)
+    	Connection connection = DBConnection.getDBConnection();
+		PreparedStatement pStatementmt = connection.prepareStatement("delete from registrations where employee_id = ?"
+				+ "and event_id = ?");
+		pStatementmt.setInt(1, employeeId);
+		pStatementmt.setInt(2, eventId);
+		int result = pStatementmt.executeUpdate();
+		if (result> 0){
 			return true;
-		else
+		}
+		else{
 			return false;
+		}
     	
     }
 }
