8406daedb21a15b5a08bf0decdca50a0b4980391
kanishkabajaj1108 <kanishkabajaj1108@gmail.com>
2018-07-14 14:20:37 +0530

    fooddb

--- EventManagementSystem/src/com/project/dao/FoodDatabaseDao.java
+++ EventManagementSystem/src/com/project/dao/FoodDatabaseDao.java
@@ -1,8 +1,9 @@
 package com.project.dao;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.project.pojo.FoodDatabase;
 
 public interface FoodDatabaseDao {
-	ArrayList<FoodDatabase>  getFoodList();
+	ArrayList<FoodDatabase>  getFoodList() throws ClassNotFoundException, SQLException;
 }
