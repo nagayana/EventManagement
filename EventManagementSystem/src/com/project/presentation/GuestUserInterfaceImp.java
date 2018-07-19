7ef5affaf2479d9d5e43be88442e1b872efa13e5
nagayana <abhimanyuprabhakar1994@gmail.com>
2018-07-19 09:12:32 +0530

    guest user interface

--- EventManagementSystem/src/com/project/presentation/GuestUserInterfaceImp.java
+++ EventManagementSystem/src/com/project/presentation/GuestUserInterfaceImp.java
@@ -14,7 +14,7 @@ import com.project.pojo.Event;
 import com.project.service.EventServiceImp;
 import com.project.service.RegisterServiceImp;
 
-public class GuestUserInterfaceImp implements GuestUserInterface {
+public class GuestUserInterfaceImp implements GuestUserInterface { 
 	
 	@Override
 	public void showMenu(Employee employee)
